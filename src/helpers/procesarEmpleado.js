8ae783940a79dd2673e9aeb2be38360b6b664ab2
IsmaelAsimbaya <85204681+IsmaelAsimbaya@users.noreply.github.com>
2022-09-03 17:15:59 -0500

    taller30

---
+++ src/helpers/procesarEmpleado.js
@@ -0,0 +1,12 @@
+
+const obtenerEmpleado = async (id) => {
+    return await obtenerEmpleadoAPI(id);
+}
+
+const obtenerEmpleadoAPI = async (id) => {
+    const data = await fetch(`http://localhost:8085/API/Nomina/V1/empleados/${id}`).then(r => r.json())
+    console.log(data)
+    return data
+}
+
+export default obtenerEmpleado
